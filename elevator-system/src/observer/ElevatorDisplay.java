7ffeb0b3c1de2407c786ab1f8205b573ab5555bd
Yash <yash.amrahs@gmail.com>
2025-09-15 21:32:18 +0530

    Design Elevator System

--- elevator-system/src/observer/ElevatorDisplay.java
+++ elevator-system/src/observer/ElevatorDisplay.java
@@ -1,4 +1,16 @@
 package observer;
 
-public class ElevatorDisplay {
+import enums.ElevatorState;
+import models.Elevator;
+
+public class ElevatorDisplay implements ElevatorObserver {
+    @Override
+    public void onElevatorStateChange(Elevator elevator, ElevatorState elevatorState) {
+        System.out.println("Elevator " + elevator.getId() + " state changed to " + elevatorState);
+    }
+
+    @Override
+    public void onElevatorFloorChange(Elevator elevator, int floor) {
+        System.out.println("Elevator " + elevator.getId() + " moved to floor " + floor);
+    }
 }
--- elevator-system/src/observer/ElevatorObserver.java
+++ elevator-system/src/observer/ElevatorObserver.java
@@ -1,4 +1,11 @@
 package observer;
 
+import enums.ElevatorState;
+import models.Elevator;
+import models.Floor;
+
 public interface ElevatorObserver {
+    void onElevatorStateChange(Elevator elevator, ElevatorState elevatorState);
+
+    void onElevatorFloorChange(Elevator elevator, int floor);
 }
